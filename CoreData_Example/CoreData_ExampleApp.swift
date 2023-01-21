4bd9b1b59f5286e0dc7dbeb9f32f0518bdfcd4d0
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-21 12:19:00 -0600

    Initial Commit

---
+++ CoreData_Example/CoreData_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_ExampleApp.swift
+//  CoreData_Example
+//
+//  Created by Renato Oliveira Fraga on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
